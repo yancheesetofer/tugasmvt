1f8f0019955d288f06725fc7f3a3b4edf804cf7f
yanchristofers <yanchristofers@gmail.com>
2022-09-14 14:50:49 +0700

    create proper views and routing functions

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        'list_barang' : data_catalog_item,
+        'nama': 'Yan Christofer S.',
+        'npm' : '2106752464'
+    }
+    return render(request, "katalog.html",context)
